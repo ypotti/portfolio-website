3d553a0451b4771fe9ad882fce98c3bcae5b7f12
ypotti <yaswanth456p@gmail.com>
2022-05-03 09:24:08 +0530

    Added content and style to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,55 @@
 import React from "react";
 import "./style.css";
 import hero from "../assests/hero_image";
+import { FiDownload } from "react-icons/fi";
+import Typewriter from "typewriter-effect";
+import logo from "../assests/logo.svg";
 
 const Home = () => {
   return (
-    <div className="w-100 bg-dark-blue h-100vh text-light d-flex justify-content-start align-items-end ps-4">
-      <div className="align-self-center">
-        <h1>
-          Hey, I'm <span className="pink-text">Yaswanth Potti</span>
-        </h1>
-        <h1>
-          a self taught{" "}
-          <span className="pink-text">Frontend Web developer</span>
-        </h1>
+    <div className="w-100 bg-dark-blue height-home text-light d-flex flex-column justify-content-start justify-content-lg-end">
+      <div className="d-md-none p-5">
+        <img src={logo} alt="yash-pic" className="main-logo-small" />
       </div>
-      <div className="d-none d-lg-block">
-        <img src={hero} alt="image" className="hero-image" />
+      <div className="d-flex justify-content-end align-items-end ps-4 mt-auto mb-auto">
+        <div className="align-self-center text-box">
+          <h1>
+            <div className="mb-3">Hey, I Am</div>
+            <div className="role-text ms-1">
+              <Typewriter
+                options={{ loop: true }}
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString("Yaswanth")
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString("Web Debelo")
+                    .pauseFor(200)
+                    .deleteChars(4)
+                    .typeString("veloper")
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString("UI Designer")
+                    .pauseFor(2000)
+                    .start();
+                }}
+              />
+            </div>
+          </h1>
+          <p className="coloured-text mt-4">
+            Stick Around to know more about me
+          </p>
+          <div className="mt-4">
+            <button className="butn mb-2">Contact Me</button>
+            <button className="butn-outline">
+              <FiDownload className="me-2" />
+              Resume
+            </button>
+          </div>
+        </div>
+        <div className="d-none d-lg-block">
+          <img src={hero} alt="hero" className="hero-image" />
+        </div>
       </div>
     </div>
   );
