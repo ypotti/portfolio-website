cdbc40024b19644f6b8bca381d1ffd7e76224031
ypotti <yaswanth456p@gmail.com>
2022-05-06 08:37:09 +0530

    Added buttons functionality to change projects

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,9 +1,28 @@
-import React from "react";
-import covidProjectImage from "../assests/covid-tracker-group.svg";
+import React, { useState } from "react";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { ImArrowLeft2, ImArrowRight2 } from "react-icons/im";
+import { projects } from "../constants";
 
 const Work = () => {
+  const [selectedIndex, setSelectedIndex] = useState(1);
+
+  const selectedProject = projects[selectedIndex];
+
+  const handleLeftClick = () => {
+    let targetIndex = selectedIndex - 1;
+    if (targetIndex < 0) {
+      targetIndex = projects.length - 1;
+    }
+    setSelectedIndex(targetIndex);
+  };
+  const handleRightClick = () => {
+    let targetIndex = selectedIndex + 1;
+    if (targetIndex >= projects.length) {
+      targetIndex = 0;
+    }
+    setSelectedIndex(targetIndex);
+  };
+
   return (
     <div className="bg-next-dark-blue ps-4 pt-5 pb-5 pe-5 d-flex flex-column justify-content-start">
       <div className="w-100 text-light pb-5" data-aos="fade-right">
@@ -20,40 +39,37 @@ const Work = () => {
         className="d-flex flex-column flex-lg-row  text-light justify-content-between align-items-center"
         data-aos="fade-right"
       >
-        <div className="order-0 order-lg-1 mb-3 floating">
-          <img
-            src={covidProjectImage}
-            alt="Covid-Project"
-            className="col-12 col-md-9 col-lg-12"
-          />
+        <div className="order-0 order-lg-1 mb-3 floating" data-aos="fade-right">
+          {selectedProject.pic}
         </div>
         <div className="order-1 order-lg-0 ms-4">
           <div className="d-flex align-items-center mb-5">
-            <button className="arrow-button me-3">
+            <button className="arrow-button me-3" onClick={handleLeftClick}>
               <ImArrowLeft2 className="butn-icon" />
             </button>
-            <div>01/01</div>
-            <button className="arrow-button ms-3">
+            <div>{`0${selectedProject.id} / 0${projects.length}`}</div>
+            <button className="arrow-button ms-3" onClick={handleRightClick}>
               <ImArrowRight2 className="butn-icon" />
             </button>
           </div>
-          <h1 className="mb-4">Covid Cases Tracker</h1>
-          <div className="d-flex align-items-center mb-3">
-            <div className="line-small-div "></div>
-            <div className="section-heading ps-3">React Application</div>
+          <div data-aos="fade-right">
+            <h1 className="mb-4">{selectedProject.name}</h1>
+            <div className="d-flex align-items-center mb-3">
+              <div className="line-small-div "></div>
+              <div className="section-heading ps-3">
+                {selectedProject.subtitle}
+              </div>
+            </div>
+            <div className="coloured-text mb-4 text-width">
+              {selectedProject.description}
+            </div>
+            <button className="butn">
+              <a className="a" href={selectedProject.url}>
+                Visit Site
+              </a>
+              <MdOutlineKeyboardArrowRight className="ms-2" />
+            </button>
           </div>
-          <div className="coloured-text mb-4 text-width">
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
-            believable.
-          </div>
-          <button className="butn">
-            <a className="a" href="#">
-              Visit Site
-            </a>
-            <MdOutlineKeyboardArrowRight className="ms-2" />
-          </button>
         </div>
       </div>
     </div>
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 import { BiBriefcaseAlt, BiHomeAlt } from "react-icons/bi";
 import { FiUser, FiMail } from "react-icons/fi";
+import covidProjectImage from "./assests/covid-tracker-group.svg";
+import mailProjectImage from "./assests/Mail-client-Group.svg";
 
 export const navItems = [
   {
@@ -17,5 +19,42 @@ export const navItems = [
   {
     name: "Connect",
     icon: <FiMail className="mobile-nav-icon" />,
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    name: "Covid Cases Tracker",
+    subtitle: "React Application",
+    description: `There are many variations of passages of Lorem Ipsum available, but
+    the majority have suffered alteration in some form, by injected
+    humour, or randomised words which don't look even slightly
+    believable.`,
+    url: "#",
+    pic: (
+      <img
+        src={covidProjectImage}
+        alt="Covid-Project"
+        className="col-12 col-md-9 col-lg-12"
+      />
+    ),
+  },
+  {
+    id: 2,
+    name: "Mail client App",
+    subtitle: "MERN Application",
+    description: `There are many variations of passages of Lorem Ipsum available, but
+    the majority have suffered alteration in some form, by injected
+    humour, or randomised words which don't look even slightly
+    believable.`,
+    url: "#",
+    pic: (
+      <img
+        src={mailProjectImage}
+        alt="Mail-Project"
+        className="col-12 col-md-9 col-lg-12"
+      />
+    ),
   },
 ];
